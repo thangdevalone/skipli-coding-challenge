3821e461ac8765a6c8e9b1f07f920d2e5ccff2c8
ThangNguyen <101063068+thangdevalone@users.noreply.github.com>
2025-06-13 09:05:07 +0700

    :fix: resolve build

---
+++ employee-task-management/src/app/confirm-employee/layout.tsx
@@ -0,0 +1,14 @@
+import { Suspense } from 'react'
+function EmployeeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Suspense>
+      {children}
+    </Suspense>
+  );
+}
+
+export default RootLayout;
